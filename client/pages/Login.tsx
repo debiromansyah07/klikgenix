ba9ba5e7a428e16f38d5bd8588b9e6ec3035e1fd
debiromansyah07 <debiromansyah07@gmail.com>
2025-07-04 19:06:13 +0700

    Fix final Login & Register layout and syntax

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -60,7 +60,7 @@ export default function Login() {
         <div className="block lg:hidden text-center mb-6">
           <img
             src="https://cdn.builder.io/o/assets%2Fec2d43fcf8b54a079080fd57b2b293e8%2F5105ee43038e43c1a5e35d9df158470e?alt=media&token=d87a45ad-fc03-472a-bc02-8eeab82821c8&apiKey=ec2d43fcf8b54a079080fd57b2b293e8
-          alt="KlixGenix Logo"
+          alt="logo"
             className="w-16 h-16 mx-auto mb-2 rounded-2xl shadow-xl object-cover"
           />
           <h2 className="text-lg text-gray-300">Masuk ke akun Anda</h2>
