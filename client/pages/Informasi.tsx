c1404e83174ccb87af1026fcc4cde6c3b6a65839
debiromansyah07 <debiromansyah07@gmail.com>
2025-07-05 00:46:36 +0700

    Add Informasi page to fix build error

---
+++ client/pages/Informasi.tsx
@@ -0,0 +1,12 @@
+// client/pages/Informasi.tsx
+
+import React from "react";
+
+export default function Informasi() {
+  return (
+    <div>
+      <h1>Informasi</h1>
+      <p>Halaman informasi.</p>
+    </div>
+  );
+}
